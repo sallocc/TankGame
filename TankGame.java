740b4d61e1351946aea7dd2fbaddf36ac6c072ba
sallocc <47262957+sallocc@users.noreply.github.com>
2022-02-01 17:30:58 -0500

    Add files via upload

---
+++ TankGame.java
@@ -0,0 +1,17 @@
+import java.lang.Thread;
+import java.lang.InterruptedException;
+import javax.swing.*;
+
+public class TankGame {
+   public static void main(String[] args) {
+      JFrame frame = new JFrame("Tank Game");
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setSize(515, 535);
+      DrawGame gameDrawer = new DrawGame();
+      gameDrawer.setFrame(frame);
+      frame.add(gameDrawer);
+      gameDrawer.initializeWindow();
+      gameDrawer.flipAnimation();
+      frame.setVisible(true);
+   }
+}
